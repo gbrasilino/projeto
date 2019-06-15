1f336c42d22a66d169cd9d9547bf3333c7e6fb33
Jose Roberto <jrcampos82@gmail.com>
2019-06-14 20:39:45 -0400

    Update Beto

--- br.com.cmd/src/main/java/br/com/cmd/autenticate/Login.java
+++ br.com.cmd/src/main/java/br/com/cmd/autenticate/Login.java
@@ -1,5 +1,41 @@
 package br.com.cmd.autenticate;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.HibernateException;
+
+import br.com.cmd.jpa.Conexao;
+import br.com.cmd.model.UserAccount;
+
 public class Login {
+	private static Logger LOG = LogManager.getLogger(Login.class.getName());
+	private EntityManager em;
+	
+	public Login() {
+		em = Conexao.getEntity();
+	}
+	
+	public boolean getLogin(String user, String pwd) {
+		
+		String sqlLogin = "FROM UserAccount as usuario WHERE usuario.email = :user AND usuario.senha = :senha";
+		try {
+			Query result = em.createQuery(sqlLogin);
+			List<UserAccount> resultList = result.getResultList();
+			if (resultList.size() > 0) return true;
+		} catch (HibernateException e) {
+			LOG.error("Erro localizando usuário!");
+			LOG.error(e.getMessage());
+		} finally {
+			em.close();
+		}
+		
+		return false;
+	}
+	
 
 }
